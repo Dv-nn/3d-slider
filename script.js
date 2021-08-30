e935e5604a2c9fe5e952a629ba70f0c1fbcf59d8
NN <Dv-nn-01@mail.ru>
2021-08-30 17:55:57 +0300

    3d slider

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var swiper =  new Swiper(".gallery", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+   loop: true,
+});
+
